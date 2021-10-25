6ceedcde843f1ea8873164053a82681c960617e3
Hotu Nga Morehu Elkington <hotunz@gmail.com>
2021-10-25 14:18:32 +0100

    HE - Changing mongoose connection slightly

--- server/database/mongodb.js
+++ server/database/mongodb.js
@@ -3,12 +3,21 @@ const mongoose = require('mongoose');
 const whichDB = process.env.NODE_ENV === 'test' ? process.env.TEST_DB : process.env.DB;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ojco1.mongodb.net/${whichDB}?retryWrites=true&w=majority`;
 
-// mongoose.Promise = global.Promise;
+mongoose.Promise = global.Promise;
+
 mongoose.connect(uri); 
 const connection = mongoose.connection
-    .once('open', () => console.log('Connected!'))
-    .on('error', (error) => {
-        console.warn('Error : ',error);
-    });
+	.once('open', () => {
+		if(process.env.NODE_ENV === 'test') {
+			console.log(process.env.NODE_ENV)
+			console.log('Test DB connection established!')
+		} else {
+			console.log(process.env.NODE_ENV)
+			console.log('DB connection established!')
+		}
+	})
+	.on('error', (error) => {
+			console.warn('Error : ',error);
+	});
 
 module.exports = connection;
