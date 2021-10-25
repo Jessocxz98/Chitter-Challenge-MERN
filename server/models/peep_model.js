11aa0df6bc5b6767369f270b302df04c3d528312
Hotu Nga Morehu Elkington <hotunz@gmail.com>
2021-10-25 11:26:33 +0100

    HE - Adding Peep model

---
+++ server/models/peep_model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PeepSchema = new Schema(
+  {
+    text: {
+      type: String,
+      required: true
+    }
+  },
+  { timestamps: true }
+)
+
+module.exports = mongoose.model('Peep', PeepSchema);
