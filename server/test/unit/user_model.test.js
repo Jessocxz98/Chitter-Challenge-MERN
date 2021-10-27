0772dce53b17648610d4f7798a69ae71d19ca03c
Hotu Nga Morehu Elkington <hotunz@gmail.com>
2021-10-27 10:56:46 +0100

    HE - Trimming white space form username

--- server/test/unit/user_model.test.js
+++ server/test/unit/user_model.test.js
@@ -3,11 +3,22 @@ const expect = require('chai').expect;
 const User = require('../../database/models/user_schema');
 
 describe('User model', () => {
-  it('is invalid if no username is provided', () => {
-    let user = new User();
+  describe('username', () => {
+    it('is invalid if no username is provided', () => {
+      let user = new User();
+  
+      user.validate((error) => {
+        expect(error.errors.username).to.exist;
+      })
+    })
 
-    user.validate((error) => {
-      expect(error.errors.username).to.exist;
+
+    it('trims whitespace', () => {
+      let user = new User({ username: '   user1  ' });
+      expect(user.username).to.equal('user1')
     })
   })
+
+
+
 })
