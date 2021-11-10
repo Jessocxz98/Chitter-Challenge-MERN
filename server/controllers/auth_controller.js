b654d688901566e4b33db2483c7323b6af42e4e7
Hotu Nga Morehu Elkington <hotunz@gmail.com>
2021-11-10 13:15:39 +0000

    cHE - Creating cookie on login

--- server/controllers/auth_controller.js
+++ server/controllers/auth_controller.js
@@ -1,4 +1,9 @@
 const UserModel = require('../database/models/User')
+const jwt = require('jsonwebtoken');
+
+const createToken = (id) => {
+  return jwt.sign({ id }, `${process.env.SECRET_KEY}`)
+}
 
 module.exports.signup_post = async (req, res) => {
   const { username, email, password} = req.body
@@ -21,10 +26,11 @@ module.exports.signup_post = async (req, res) => {
 
 module.exports.login_post = async (req, res) => {
   const { email, password } = req.body;
-
   try {
     const user = await UserModel.login(email, password)
-    res.status(200).json({ message: 'Login successful!' })
+    const token = createToken(user._id);
+    res.cookie('jwt', token, { httpOnly: true })
+    res.status(200).json({ user: user._id, message: 'Login successful!' })
   }
   catch (err){
     res.status(401).json({ message: err.message})
