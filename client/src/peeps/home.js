f61b352ea486e680fe477a5d3030bc402e870792
Te Hotu <hotunz@gmail.com>
2021-12-02 10:39:53 +0000

    HE - Adding setTimeOut to fetch api call

--- client/src/peeps/home.js
+++ client/src/peeps/home.js
@@ -11,20 +11,22 @@ export const Home = () => {
       if (document.cookie.length > 0) return setIsLoggedIn(true);
     }
 
-    fetch('http://localhost:5000/peeps/')
+    // Need to set this as a webhook to subscribe to updates on my api for now this is ok
+    setTimeout(() => {
+      fetch('http://localhost:5000/peeps/')
       .then(res => {
         return res.json();
       })
       .then(data => {
-        console.log(data)
         setPeeps(data)
       })
       .catch(err => {
         console.log(err)
       })
-    
+    }, 1000)
+
     checkUserLoggedIn()
-  }, [])
+  }, [peeps])
 
 
   
