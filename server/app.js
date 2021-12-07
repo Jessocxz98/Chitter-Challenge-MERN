74698266ba0098e100b5db0e4cc0aeed6916cda4
Te Hotu <hotunz@gmail.com>
2021-12-07 11:17:47 +0000

    HE - Removing proxy

--- server/app.js
+++ server/app.js
@@ -28,11 +28,13 @@ database.dbConnect()
 
 // Code for deployment starts
 
-app.use(express.static(path.resolve(__dirname, "./client/build")));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.resolve(__dirname, "./client/build")));
 
-app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-});
+  app.get("*", function (request, response) {
+    response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+  });
+}
 
 // Code for deployment ends
 
