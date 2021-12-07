398ed721405ea91fe07fdef191bf3b0c627bf16c
Te Hotu <hotunz@gmail.com>
2021-12-07 21:50:26 +0000

    HE - Another try

--- server/app.js
+++ server/app.js
@@ -30,7 +30,7 @@ database.dbConnect()
 // Code for deployment starts
   app.use(express.static(path.resolve(__dirname, "build")));
 
-  app.get("/*", function (req, res) {
+  app.get("/", function (req, res) {
     res.sendFile(path.resolve(__dirname, "build", "index.html"));
   });  
 
