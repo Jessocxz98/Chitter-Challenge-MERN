6ebac390d75b6cfadcad579d9095a6ebf64b2783
Te Hotu <hotunz@gmail.com>
2021-12-08 09:48:24 +0000

    HE - trying different script

--- server/app.js
+++ server/app.js
@@ -24,15 +24,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-database.dbConnect()
-  .on('error', (error) => console.log('Error: ', error))
+database.dbConnect().on('error', (error) => console.log('Error: ', error))
 
 // Code for deployment starts
-  app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 
-  app.get("/", function (req, res) {
-    res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-  });  
+app.get("/", function (req, res) {
+  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});  
 
 // Code for deployment ends
 
