67303f6348a5e1afde65d7bc09f5f0f9c0542f38
Te Hotu <hotunz@gmail.com>
2021-12-09 17:45:41 +0000

    HE - Restarting deployment

--- app.js
+++ app.js
@@ -32,18 +32,17 @@ app.use((req, res, next) => {
 
 database.dbConnect().on('error', (error) => console.log('Error: ', error))
 
-
-
 app.use('/api/peeps', peepRouter);
 app.use('/api/users', userRouter);
 
 // Code for deployment starts
-app.use(express.static(path.resolve(__dirname, "./client/build")));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.resolve(__dirname, "./client/build")));
+}
 
-app.get("*", function (request, response) {
+app.get("/*", function (request, response) {
   response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
-
 // Code for deployment ends
 
 app.listen(port, () => {
