4fa687e2f19492c133f03b3cb3d58d7ff21bf348
Te Hotu <hotunz@gmail.com>
2021-12-09 20:17:47 +0000

    HE - Clutching at straws

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use('/api/users', userRouter);
 // Code for deployment starts
 
 app.use('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './client/public/index.html'));
+  res.sendFile(path.resolve(__dirname, './client/public', 'index.html'));
 });
 // Code for deployment ends
 
