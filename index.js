6c0978cc546e28f8df462fbdf2d42a991a4e6e16
Te Hotu <hotunz@gmail.com>
2021-12-13 14:09:50 +0000

    HE - index.js

--- index.js
+++ index.js
@@ -32,13 +32,14 @@ app.use('/api/peeps', peepRouter);
 app.use('/api/users', userRouter);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.resolve(__dirname, "client/build")));
+  app.use(express.static(path.join(__dirname, "client/build")));
 
   app.get("*", function (request, response) {
-    response.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    response.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
   });  
 };
 
 app.listen(port, () => {
+  console.log(process.env.NODE_ENV)
   console.log(`Listening on port: ${port}`)
 });
